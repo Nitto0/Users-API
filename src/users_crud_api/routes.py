bee62abad138652329f50563c8df9d1f3331c46a
Makar <makarsidorevic@gmail.com>
2025-10-01 00:54:28 +0300

    Fix create route

--- src/users_crud_api/routes.py
+++ src/users_crud_api/routes.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify, request
+import sqlite3
+from flask import Flask, jsonify, request, abort
 import models
 from datetime import datetime, UTC
 
@@ -14,24 +15,31 @@ def read_users():
     users = [{'id': user[0], 'name': user[1], 'email': user[2], 'created_at': user[3]}
              for user in cur.fetchall()]
 
-    return jsonify(users)
+    return jsonify(users), 200
 
 
 @app.route("/api/users", methods=['POST'])
 def create_user():
     new_user = request.json
+
+    if not new_user or 'name' not in new_user:
+        abort(400, description="Должно быть поле name")
+
     created_at = datetime.now(UTC).isoformat()
     new_user['created_at'] = created_at
 
     users_db = models.init_users_db()
     cur = users_db.cursor()
 
-    cur.execute("INSERT INTO Users (name, email, created_at) VALUES (?, ?, ?)",
-                (new_user['name'], new_user['email'], new_user['created_at']))
-    users_db.commit()
+    try:
+        cur.execute("INSERT INTO Users (name, email, created_at) VALUES (?, ?, ?)",
+                    (new_user['name'], new_user['email'], new_user['created_at']))
+        users_db.commit()
 
-    return jsonify({
-        'name': new_user['name'],
-        'email': new_user['email'],
-        'created_at': new_user['created_at']
-    })
+        return jsonify({
+            'name': new_user['name'],
+            'email': new_user['email'],
+            'created_at': new_user['created_at']
+        })
+    except sqlite3.IntegrityError:
+        abort(409, description="email должен быть уникальным")
