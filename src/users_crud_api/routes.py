812e4f0aa9f22f1f6121e35c8c038154f452c73d
Makar <makarsidorevic@gmail.com>
2025-10-03 01:21:56 +0300

    Wad added delete and update routes

--- src/users_crud_api/routes.py
+++ src/users_crud_api/routes.py
@@ -18,12 +18,33 @@ def read_users():
     return jsonify(users), 200
 
 
+@app.route("/api/users/<int:user_id>", methods=['GET'])
+def read_user(user_id):
+    # Валидация здесь
+
+    users_db = models.init_users_db()
+    cur = users_db.cursor()
+    cur.execute("SELECT * FROM Users WHERE id = ?", (user_id, ))
+
+    user = cur.fetchone()
+    if not user:
+        abort(404, description="This user does not exists")
+    return jsonify({
+        'id': user[0],
+        'name': user[1],
+        'email': user[2],
+        'created_at': user[3]
+    }), 200
+
+
 @app.route("/api/users", methods=['POST'])
 def create_user():
     new_user = request.json
 
     if not new_user or 'name' not in new_user:
-        abort(400, description="Должно быть поле name")
+        abort(400, description="You should write a name")
+
+    # Валидация здесь
 
     created_at = datetime.now(UTC).isoformat()
     new_user['created_at'] = created_at
@@ -32,7 +53,7 @@ def create_user():
     cur = users_db.cursor()
 
     try:
-        cur.execute("INSERT INTO Users (name, email, created_at) VALUES (?, ?, ?)",
+        cur.execute('INSERT INTO Users (name, email, created_at) VALUES (?, ?, ?)',
                     (new_user['name'], new_user['email'], new_user['created_at']))
         users_db.commit()
 
@@ -40,6 +61,64 @@ def create_user():
             'name': new_user['name'],
             'email': new_user['email'],
             'created_at': new_user['created_at']
-        })
+        }), 201
     except sqlite3.IntegrityError:
-        abort(409, description="email должен быть уникальным")
+        abort(409, description="Email should be unique")
+
+
+@app.route("/api/users/<int:user_id>", methods=['DELETE'])
+def delete_user(user_id):
+    # Валидация здесь
+
+    users_db = models.init_users_db()
+    cur = users_db.cursor()
+    cur.execute("SELECT * FROM Users WHERE id = ?", (user_id, ))
+
+    user = cur.fetchone()
+    if not user:
+        abort(404, description="This user does not exists")
+
+    cur.execute("DELETE FROM Users WHERE id = ?", (user_id, ))
+    users_db.commit()
+
+    return jsonify({'message': 'User deleted successfully'}), 200
+
+
+@app.route("/api/users/<int:user_id>", methods=['PATCH'])
+def update_user(user_id):
+    # Валидация здесь (user_id)
+
+    updated_data = request.json
+    if not updated_data:
+        abort(400, description="No data to update")
+
+    users_db = models.init_users_db()
+    cur = users_db.cursor()
+
+    cur.execute("SELECT * FROM Users WHERE id = ?", (user_id, ))
+    user = cur.fetchone()
+
+    if not user:
+        abort(404, description="This user does not exists")
+
+    new_name = updated_data.get('name', user[1])
+    new_email = updated_data.get('email', user[2])
+
+    # Валидация name и email
+
+    try:
+        cur.execute("UPDATE Users SET name = ?, email = ? WHERE id = ?",
+                    (new_name, new_email, user_id))
+        users_db.commit()
+
+        cur.execute("SELECT * FROM Users WHERE id = ?", (user_id, ))
+        user = cur.fetchone()
+
+        return jsonify({
+            'id': user[0],
+            'name': user[1],
+            'email': user[2],
+            'created_at': user[3]
+        }), 200
+    except sqlite3.IntegrityError:
+        abort(409, description="Email should be unique")
